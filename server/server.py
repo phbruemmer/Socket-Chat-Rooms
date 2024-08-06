5fb7e178cd77dde8a65981224415c9c94a706861
phbruemmer <phbruemmer@gmail.com>
2024-08-06 21:03:56 +0200

    - Added more commenting - completed client thread -> working on: - server_lobby_cmd -> server lobby commands to send and process commands from client

--- server/server.py
+++ server/server.py
@@ -1,9 +1,12 @@
 import socket
+import struct
 import threading
 import time
+import argparse
 
 HOSTNAME = socket.gethostname()
 HOST = socket.gethostbyname(HOSTNAME)
+BROADCAST_PORT = 5555
 PORT = 5000
 BUFFER = 1024
 
@@ -12,9 +15,14 @@ CLIENTS = []
 SEND_BROADCAST_EVENT = threading.Event()
 TCP_CONNECTION_EVENT = threading.Event()
 WAIT_FOR_CONNECTION_EVENT = threading.Event()
+LISTEN_FOR_CMD = threading.Event()
 
 
 def broadcast_beacon():
+    """
+    - sends the Port of the Lobby server to everyone listening on BROADCAST_PORT.
+    :return:
+    """
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)     # Address Family, UDP
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, True)   # True -> Activates message broadcast ability
     """
@@ -26,8 +34,10 @@ def broadcast_beacon():
 
     try:
         while not SEND_BROADCAST_EVENT.is_set():
-            sock.sendto(msg.encode(), ('<broadcast>', PORT))
-            print(f"[broadcast] Broadcast message sent: {msg}")
+            sock.sendto(struct.pack('!I', PORT), ('<broadcast>', BROADCAST_PORT))
+                                        # ! - specifies network byte order
+                                        # I - specifies an unsigned int (4 bytes)
+            print(f"\n[broadcast] Broadcast message sent: {msg}")
             time.sleep(5)
     except Exception as e:
         print(f"[ERROR] Broadcast error: {e}")
@@ -38,40 +48,64 @@ def broadcast_beacon():
         sock.close()
 
 
+def server_lobby_cmd(conn):
+    """
+    - function to handle new client.
+    :param conn:
+    :return:
+    """
+    while True:
+        data_ = conn.recv(BUFFER).decode()
+        if not data_:
+            break
+        print(data_)
+
+
 def server_main_lobby():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind((HOST, PORT))
-    sock.listen(20)
-    print(f"[TCP] listening for connections on {HOST}:{PORT}")
-    try:
-        while not TCP_CONNECTION_EVENT.is_set():
-            sock.settimeout(1)
-            try:
+    """
+    - accepts TCP connections and starts new thread to handle new client.
+    :return:
+    """
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.bind((HOST, PORT))
+        sock.listen(20)
+        print(f"[TCP] listening for connections on {HOST}:{PORT}")
+        try:
+            while not TCP_CONNECTION_EVENT.is_set():
                 conn, addr = sock.accept()
                 print(f"[info] connection from {addr}")
                 CLIENTS.append(conn)
-            except socket.timeout:
-                continue
-    except Exception as e:
-        print(f"[ERROR] connection handling error: {e}")
-    finally:
-        TCP_CONNECTION_EVENT.set()
-        sock.close()
-        print("[info] server socket closed.")
+                server_lobby_thread = threading.Thread(target=server_lobby_cmd, args=(conn,))
+                server_lobby_thread.start()
+        except socket.timeout:
+            print("[info] socket timeout.")
+        except Exception as e:
+            print(f"[ERROR] connection handling error: {e}")
+        finally:
+            TCP_CONNECTION_EVENT.set()
+            sock.close()
+            print("[info] server socket closed.")
 
 
 def server_main():
+    """
+    main-routine:
+        - broadcast thread is always running.
+        - server_main_lobby handles own threads for the clients
+    :return:
+    """
     broadcast_thread = threading.Thread(target=broadcast_beacon)
     broadcast_thread.start()
-    while True:
-        lobby_thread = threading.Thread(target=server_main_lobby)
-        lobby_thread.start()
-        WAIT_FOR_CONNECTION_EVENT.wait()
-        WAIT_FOR_CONNECTION_EVENT.clear()
+    server_main_lobby()
 
 
 if __name__ == "__main__":
-    broadcast_beacon()
+    server_main()
+
+
+
+
+
 
 """
 # # # SERVER - STRUCTURE # # #
