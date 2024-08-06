ca8066682a81c7629243b811f3c0db3073f63f5a
phbruemmer <phbruemmer@gmail.com>
2024-08-06 20:58:01 +0200

    - Added more settings to the chat_rooms. - Fixed HOST-issue (missing -> ())

--- server/rooms/chat_rooms.py
+++ server/rooms/chat_rooms.py
@@ -1,13 +1,17 @@
 import socket
 import threading
 
-HOST = socket.gethostbyname(socket.gethostname)
+HOST = socket.gethostbyname(socket.gethostname())
 
 
 class Room:
-    def __init__(self, host=HOST, port=5000, max_users=20):
+    def __init__(self, host=HOST, port=5000, max_users=20,
+                 room_name='standard_room', global_room=False, room_password=''):
         self.host = host
         self.port = port
+        self.room_name = room_name
+        self.global_room = global_room
+        self.room_password = room_password
         self.room_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.room_socket.bind((host, port))
         self.room_socket.listen(max_users)
@@ -24,5 +28,11 @@ class Room:
             print("[info] stopping server...")
             self.room_socket.close()
 
-    def handle_client(self):
-        pass
+    def handle_client(self, room_socket):
+        with room_socket:
+            while True:
+                data = room_socket.recv(1024)
+                if not data:
+                    break
+                print(data.decode())
+                room_socket.sendall(data)
