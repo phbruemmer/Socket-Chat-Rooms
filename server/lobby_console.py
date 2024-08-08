c183e48cceccfadd139dac3135695af2fe9f5f74
phbruemmer <phbruemmer@gmail.com>
2024-08-08 15:59:50 +0200

    - began with the lobby console. - working on the commands later

---
+++ server/lobby_console.py
@@ -0,0 +1,34 @@
+import os
+import time
+
+
+class Lobby:
+    def __init__(self, command):
+        self.command = command
+        self.username = None
+
+    def cmd_username(self, new_username):
+        pass
+
+    def cmd_create(self, create_type, name, password=''):
+        pass
+
+    def cmd_join(self, room):
+        pass
+
+    def cmd_list_rooms(self):
+        pass
+
+    def process_command(self):
+        cmd_instructions = self.command.split()
+        main_cmd = cmd_instructions[0]
+        cmd_arguments = cmd_instructions.pop(0)
+        argument_length = len(cmd_arguments)
+        print(main_cmd)
+        if main_cmd == '$username' and argument_length == 1:
+            self.cmd_username(cmd_arguments)
+
+
+if __name__ == '__main__':
+    lobby = Lobby('join [name]')
+    lobby.process_command()
