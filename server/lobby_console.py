000dc12fb2b1b45ca9b03f24a1729971f9f9fda3
phbruemmer <phbruemmer@gmail.com>
2024-08-14 19:24:41 +0200

    - small changes

--- server/lobby_console.py
+++ server/lobby_console.py
@@ -1,6 +1,7 @@
 class Lobby:
     def cmd_username(self, user, new_username):
         print(new_username)
+        user.username = new_username
 
     def cmd_create(self, user, create_type, name, password=''):
         pass
@@ -11,7 +12,7 @@ class Lobby:
     def cmd_list_rooms(self, user):
         pass
 
-    def process_command(self, command, user=''):
+    def process_command(self, command, user):
         cmd_instructions = command.split()
         main_cmd = cmd_instructions[0]
         cmd_arguments = cmd_instructions.pop(-1)
