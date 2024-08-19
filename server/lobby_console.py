3340dc2a5ded4b28e9327b4934388c7658642307
phbruemmer <phbruemmer@gmail.com>
2024-08-19 23:17:32 +0200

    - Prepared room creation command in lobby_console.py and added default argument for change_room method in user.py

--- server/lobby_console.py
+++ server/lobby_console.py
@@ -1,10 +1,28 @@
+import random
+
+import rooms.chat_rooms
+
+
+used_ports = []
+
+
 class Lobby:
     def cmd_username(self, user, new_username):
         print(new_username)
         user.username = new_username
 
     def cmd_create(self, user, create_type, name, password=''):
-        pass
+        def get_available_port():
+            new_port = random.randint(3000, 3100)
+            while new_port in used_ports:
+                new_port = random.randint(3000, 3100)
+            print(new_port)
+            return new_port
+        if create_type == 'chat':
+            port = get_available_port()
+            new_room = rooms.chat_rooms.Room(port=port, room_name=name, room_password=password, admin=user)
+            new_room.start_room()
+            user.change_room(new_room)
 
     def cmd_join(self, user, room):
         pass
--- server/user.py
+++ server/user.py
@@ -1,7 +1,9 @@
+import rooms.chat_rooms
+
 class User:
     def __init__(self, username, user_id):
         self.username = username
         self.user_id = user_id
 
-    def change_room(self, room):
+    def change_room(self, room=rooms.chat_rooms.Room):
         pass
