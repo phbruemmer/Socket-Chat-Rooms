d3c136cb3519cab5dc37dfea41c4e4380c89ad81
phbruemmer <phbruemmer@gmail.com>
2024-08-25 23:23:40 +0200

    - user.py sends new room host / port to the client - NOT TRIED YET ...

--- server/user.py
+++ server/user.py
@@ -1,3 +1,7 @@
+import time
+
+from server import CLIENTS
+
 class User:
     def __init__(self, username, user_id):
         self.username = username
@@ -5,3 +9,17 @@ class User:
 
     def change_room(self, room):
         print(f"[info] changing room to {room.room_name}...")
+        conn = CLIENTS[self.user_id]
+        change_room_msg = b'$change_room$'
+        conn.send(change_room_msg)
+        valid_cmd = conn.recv(1).decode()
+        if not valid_cmd:
+            return
+        conn.send(room.host)
+        valid_cmd = conn.recv(1).decode()
+        if not valid_cmd:
+            return
+        conn.send(room.port)
+        valid_cmd = conn.recv(1).decode()
+        if not valid_cmd:
+            return
